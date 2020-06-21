8a309692d06ceb0729339a751ffa0866f0409eff
albihasani94 <albihasani94@gmail.com>
2020-06-21 02:55:41 +0200

    Some WIP

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SymmetricDS
-SymetricDS Setup for replicating through three different nodes
+
+SymetricDS Setup
+
+- [Mysql to Postgres](./mysql2postgres)
+- [Postgres to Postgres](./postgres2postgres)
+
+## Show me the code
+
+```bash
+./symadmin --engine heroku-00 create-sym-tables
+./dbimport --engine heroku-00 ../scripts/setup.sql
+./sym_service start
+./sym_service stop
+./symadmin --engine heroku-00 uninstall
+./symadmin --engine postgres-01 uninstall
+```
---
+++ postgres2postgres/scripts/setup.sql
@@ -0,0 +1,38 @@
+------------------------------------------------------------------------------
+-- Symmetric Configuration
+------------------------------------------------------------------------------
+
+insert into sym_channel
+(channel_id, processing_order, max_batch_size, enabled, description)
+values('human', 1, 100000, 1, 'human data from postgres heroku instance to postgres local instance');
+
+insert into sym_channel
+(channel_id, processing_order, max_batch_size, enabled, description)
+values('course', 1, 100000, 1, 'course data from postgres heroku instance to postgres local instance');
+
+-- insert into sym_node_group (node_group_id)
+-- values ('heroku');
+insert into sym_node_group (node_group_id)
+values ('postgres');
+
+insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action)
+values ('heroku', 'postgres', 'W');
+
+-- insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action)
+-- values ('postgres', 'heroku', 'W');
+
+insert into sym_trigger
+(trigger_id,source_table_name,channel_id, sync_on_delete, last_update_time,create_time)
+values('course','course','course', 0, current_timestamp,current_timestamp);
+
+insert into sym_trigger
+(trigger_id,source_table_name,channel_id, sync_on_delete, last_update_time,create_time)
+values('human','human','human', 0, current_timestamp,current_timestamp);
+
+insert into sym_router
+(router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
+values('heroku_2_postgres', 'heroku', 'postgres', 'default',current_timestamp, current_timestamp);
+
+insert into sym_trigger_router
+(trigger_id,router_id,initial_load_order,last_update_time,create_time)
+values('course','heroku_2_postgres', 200, current_timestamp, current_timestamp);
